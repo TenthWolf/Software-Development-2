0f061f4be9c272e136d4f8be62984db01ca04d87
Andre Fernandez <andrefernandez250@gmail.com>
2020-08-29 21:37:35 -0400

    Worked on

--- Module-01/FolderFinderFX/src/MainController.java
+++ Module-01/FolderFinderFX/src/MainController.java
@@ -12,26 +12,47 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class MainController {
 
     @FXML
-    TreeItem<String> currentParent;
+    TreeItem <String> currentParent;
 
     Path currentPath = Paths.get(System.getProperty("user.dir"));
 
     @FXML
-    private TreeView<String> tree;
-    @FXML
-    TreeItem<String> root = new TreeItem<String>(currentPath.toString());
+    TreeView <String> treeView;
+
+
+    public void initialize() throws Exception {
+        TreeItem<String> root = new TreeItem<>(currentPath.getFileName().toString());
+        currentParent = root;
+        listPath(currentPath, 0, root);
+        root.setExpanded(true);
+        treeView.setRoot(root);
+
+    }
 
 
     
 
-    public static void listPath(Path path, int depth) throws Exception {
+    public static void listPath(Path path, int depth, TreeItem<String> parent) throws Exception {
         BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
         // If this is a directory, list all directories inside
         if(attr.isDirectory()) {
+
+            // Start Directory Stream to view contents
             DirectoryStream<Path> paths = Files.newDirectoryStream(path);
+
             System.out.println(spacesForDepth(depth) + " >" + path.getFileName());
+
+            // Create new branch
+            TreeItem<String> item = new TreeItem<>(path.getFileName().toString());
+            item.setExpanded(true);
+
+            // Add branch to parent
+            parent.getChildren().add(item);
+
+
+            // Recursion to create all branches from root path
             for(Path p : paths)
-                listPath(p, depth + 1);
+                listPath(p, depth + 1, item);
         } // end if
 
     } // end listPath
@@ -46,7 +67,7 @@ public class MainController {
     } // end spacesForDepth
 
 
-    public TreeItem<String> makeBranch(String folderName, TreeItem<String> parent){
+    public static TreeItem<String> makeBranch(String folderName, TreeItem<String> parent){
         TreeItem<String> item = new TreeItem<>(folderName);
         item.setExpanded(true);
         parent.getChildren().add(item);
